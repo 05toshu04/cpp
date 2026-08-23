07fcf26cfb1ded5e09f0abbfbb2f40adebfe01ee
tushar goswami <tussygoswami01@gmail.com>
2026-08-23 11:55:21 +0530

    basics of c++

---
+++ manipulators.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int a=4;
+    cout<<"the value of a :"<<a<<endl;
+    cout<<"the value of a :"<<setw(4)<<a;
+
+
+
+    return 0;
+}
---
+++ typecasting.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // typecasting
+    float f;
+    cout<<"enter the value of f"<<endl;
+    cin>>f;
+    cout<<int(f);
+    
+    return 0; 
+}
