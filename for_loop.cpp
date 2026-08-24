d29daaabddb503c1e0606010f746b04a185ae239
tushar goswami <tussygoswami01@gmail.com>
2026-08-24 11:57:31 +0530

    use case of for loop

---
+++ for_loop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout<<"even numbers from (0-100):"<<endl;
+    for(int i=0;i<=100;i=i+2){
+        cout<<i<<",";
+    }
+
+    return 0;
+}
