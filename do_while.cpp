7bc551b8b7f831eaf58a54d0c7fecc77e0d7a028
tushar goswami <tussygoswami01@gmail.com>
2026-08-30 11:29:50 +0530

    use case of do while loop

---
+++ do_while.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //  in do while loop the loop will be run atleast one time then check the condition it is the major difference between while loop and do while loop
+
+    int i=101;
+    do{
+        cout<<i<<endl;
+        i++;
+
+    }
+    while(i<=100);
+
+
+    return 0;
+}
